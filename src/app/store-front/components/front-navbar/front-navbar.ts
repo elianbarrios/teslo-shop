f095b166927a04c224967182a9a9b43f57bce701
elianbarrios <elianbarrios1707@outlook.com>
2026-07-29 15:41:38 -0600

    add initial routes

---
+++ src/app/store-front/components/front-navbar/front-navbar.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-front-navbar',
+  imports: [],
+  templateUrl: './front-navbar.html'
+})
+export class FrontNavbar {}
---
+++ src/app/store-front/layouts/store-front-layout/store-front-layout.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'app-store-front-layout',
+  imports: [RouterOutlet],
+  templateUrl: './store-front-layout.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
+})
+export class StoreFrontLayout {
+
+}
