00042e36db2a67a0216dfc3932db498d63bf190a
elianbarrios <elianbarrios1707@outlook.com>
2026-08-12 08:17:04 -0600

    add navbar and Montserrat font

--- src/app/store-front/components/front-navbar/front-navbar.ts
+++ src/app/store-front/components/front-navbar/front-navbar.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-front-navbar',
-  imports: [],
+  selector: 'front-navbar',
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './front-navbar.html'
 })
 export class FrontNavbar {}
--- src/app/store-front/layouts/store-front-layout/store-front-layout.ts
+++ src/app/store-front/layouts/store-front-layout/store-front-layout.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import { FrontNavbar } from "../../components/front-navbar/front-navbar";
 
 @Component({
   selector: 'app-store-front-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FrontNavbar],
   templateUrl: './store-front-layout.html',
   changeDetection: ChangeDetectionStrategy.Eager,
 })
